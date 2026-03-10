f0411f5c5373c3b522d8ab4049581d8f8312bf19
Caio Costa <caio.costa@grupocriar.com>
2026-03-10 17:55:14 -0300

    att

--- app/src/main/java/com/dev/caiovinicius/conversordemoedas/MainActivity.kt
+++ app/src/main/java/com/dev/caiovinicius/conversordemoedas/MainActivity.kt
@@ -9,16 +9,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
+import com.dev.caiovinicius.conversordemoedas.databinding.ActivityMainBinding
+import com.dev.caiovinicius.conversordemoedas.ui.CurrencyTypesAdapter
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     private val viewModel by viewModels<CurrencyExchangeViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -29,11 +34,8 @@ class MainActivity : AppCompatActivity() {
             launch {
                 viewModel.currencyTypes.collect { result ->
                     result.onSuccess { currencyTypes ->
-                        Toast.makeText(
-                            this@MainActivity,
-                            currencyTypes.size.toString(),
-                            Toast.LENGTH_LONG
-                        ).show()
+                        binding.spnFromExchange.adapter = CurrencyTypesAdapter(currencyTypes)
+                        binding.spnToExchange.adapter = CurrencyTypesAdapter(currencyTypes)
                     }.onFailure {
                         Toast.makeText(
                             this@MainActivity, it.message,
